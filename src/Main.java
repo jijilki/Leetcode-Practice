ebae43bad52911e4ad0cec69f534602866e681ab
Jijil Kakkadathu <jijilki@gmail.com>
2022-08-20 16:35:50 +0100

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import pattern.LongestPalindrome;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+    public static void main(String[] args) {
+        LongestPalindrome lp = new LongestPalindrome();
+        String palindrome = lp.longestPalindrome("malayalam");
+        logger.log(Level.INFO, "Longest palindrome in String is {0} " , palindrome);
+
+    }
+}
