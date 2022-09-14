b68a8544e60b7cd59716fabf0cb43bbee2e733d5
Jijil Kakkadathu <jijilki@gmail.com>
2022-09-14 17:10:27 +0100

    Simple Single Thread Singleton pattern

---
+++ src/com/jijil/designpatterns/Singleton.java
@@ -0,0 +1,49 @@
+package com.jijil.designpatterns;
+
+public class Singleton {
+
+    //Static Class variable
+    private static Singleton instance ;
+
+    public String value;
+
+
+    //private constructor
+    private Singleton(String value) {
+
+        try {
+            Thread.sleep(1000);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        this.value = value;
+
+
+    }
+
+    //Static method to get Instance of object. Returns existing instance if any
+    public static Singleton getInstance(String val){
+        if(instance == null) {
+            instance = new Singleton(val);
+        }
+
+        return  instance;
+    }
+
+
+}
+
+class TestClient {
+
+
+    public static void main(String[] args) {
+
+        Singleton singleton = Singleton.getInstance("Foo");
+        Singleton anotherSingleton = Singleton.getInstance("Boo");
+
+        System.out.println(singleton.value);
+        System.out.println(anotherSingleton.value);
+    }
+
+}
